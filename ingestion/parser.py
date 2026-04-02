97e1a4e0ad4ec0f114eaefb402045b6bebba53a9
n.maheshwar <nmnm.1.2007@gmail.com>
2026-04-02 13:43:21 +0530

    Project Structure and initial code

--- ingestion/parser.py
+++ ingestion/parser.py
@@ -9,7 +9,7 @@
 from typing import Type, Any, Generator
 
 
-def read_logs(log_file:str)-> Generator[str, Any, None]:
+def read_logs(log_file: str) -> Generator[str, Any, None]:
     """
     Parse the log file
     :param log_file:
@@ -19,7 +19,7 @@ def read_logs(log_file:str)-> Generator[str, Any, None]:
     try:
         with open(log_file, 'r') as f_obj:
             for line in f_obj.readlines():
-                 yield line
+                yield line
     except FileNotFoundError:
         raise FileNotFoundError("Log file not found, check LOG_FILE in .env")
     except IOError:
@@ -28,34 +28,46 @@ def read_logs(log_file:str)-> Generator[str, Any, None]:
         raise e
 
 
-
-
-def parse_log_segment(line:str):
+def parse_log_segment(line: str) -> Dict[str, Any]:
     """
-
+    get the line from log file and parse the message segment
     :param line:
     :return:
     """
-    ret_dict = {}
+
+    ret_dict: Dict[str, Any] = {}
+    time_stamp = msg_dir = sip_msg = from_hdr = to_hdr = call_id = \
+        content_length = ""
+
     if not line:
         return None
     line = line.strip()
-    if line.strip().startswith(' '):
+    if line.startswith((' ', '#')):
         return None
     try:
-       if line.startswith('['):
-           time_stamp = line.split(sep=" ")[0]
-           msg_dir = line.split(sep=" ")[1]
-           ret_dict[time_stamp] = time_stamp
-           ret_dict[msg_dir] = msg_dir
+        if line.startswith('['):
+            time_stamp = line.split(sep=" ")[0] if time_stamp else ""
+            msg_dir = line.split(sep=" ")[1]
+        if line.startswith(('INVITE', 'UPDATE',)):
+            sip_msg = line.split(sep=" ")[0]
+        if line.startswith(('SIP/2.0',)):
+            sip_msg = line.split(sep=" ")[1] + line.split(sep=" ")[2]
+        if line.startswith(('From',)):
+            from_hdr = line.split(sep=":")[1]
+        if line.startswith(('To',)):
+            to_hdr = line.split(sep=":")[1]
+        if line.startswith(('Call-ID',)):
+            call_id = line.split(sep=":")[1]
+        if line.startswith(('Content-Length',)):
+            content_length = line.split(sep=":")[1]
 
-
+        ret_dict.update(
+            {time_stamp:(sip_msg, from_hdr, to_hdr, call_id, content_length)})
+        return ret_dict
 
     except ValueError:
         raise Exception("line is invalid")
-        
+    except Exception as e:
+        raise e
+
     return None
-
-
-    
-
