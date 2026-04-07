f9d598d5af0b59f57e8e33d3921b14bf83b7da81
n.maheshwar <nmnm.1.2007@gmail.com>
2026-04-07 18:55:40 +0530

    files minor bugs fixed

--- ingestion/normalizer.py
+++ ingestion/normalizer.py
@@ -2,6 +2,7 @@
 class Normalizer
 
 """
+
 from datetime import datetime
 
 
@@ -10,54 +11,85 @@ class Normalizer:
         self.cleaned_msg = {}
 
     def normalize(self, parsed_msg: dict) -> dict:
+        """
+        normalize the message
+        :param parsed_msg:
+        :return:
+        """
         if parsed_msg is None:
             return {}
 
         cleaned_msg = {}
 
-        cleaned_msg["timestamp"] = self._normalize_time_stamp(
-                parsed_msg["timestamp"])
-        cleaned_msg["direction"] = self._normalize_direction(
-                parsed_msg["direction"])
+        cleaned_msg["timestamp"] = self._normalize_time_stamp(parsed_msg["timestamp"])
+        cleaned_msg["direction"] = self._normalize_direction(parsed_msg["direction"])
         cleaned_msg["sip_msg"] = self._normalize_headers(parsed_msg["sip_msg"])
         cleaned_msg["from"] = parsed_msg["from"].strip()
         cleaned_msg["to"] = parsed_msg["to"].strip()
         cleaned_msg["call_id"] = self._normalize_call_id(parsed_msg["call_id"])
         cleaned_msg["content_length"] = self._normalize_content_len(
-                parsed_msg["content_length"])
+            parsed_msg["content_length"]
+        )
 
         return cleaned_msg
 
-    def _normalize_time_stamp(self, time_stamp):
-        if isinstance(time_stamp, datetime):
-            return time_stamp.isoformat()
-        elif isinstance(time_stamp, str):
-            d = datetime.strptime(time_stamp, "%Y-%m-%d %H:%M:%S,%f")
-            return d.isoformat()
+    def _normalize_time_stamp(self, time_stamp: datetime or str) -> datetime:
+        """
+        normalize the time stamp
+        :param time_stamp:
+        :return:
+        """
+        try:
+            if isinstance(time_stamp, datetime):
+                return time_stamp.isoformat()
+            elif isinstance(time_stamp, str):
+                d = datetime.strptime(time_stamp, "%Y-%m-%d %H:%M:%S,%f")
+                return d.isoformat()
+        except Exception as e:
+            return "INVALID time stamp ::" + time_stamp + " " + e
 
-    def _normalize_direction(self, direction):
+    def _normalize_direction(self, direction: str) -> str:
+        """
+        normalize the direction
+        :param direction:
+        :return:
+        """
         if not isinstance(direction, str):
             return "default"
         if direction in ("Received", "Sending"):
             return "IN" if direction == "Received" else "OUT"
+        else:
+            return "UNKNOWN Direction ::" + direction
 
-    def _normalize_headers(self, headers):
-        SIP_METHODS = ('INVITE', 'UPDATE', 'ACK', 'BYE', 'OPTIONS')
-        SIP_RESPONSES = ('100 TRYING', '180 RINGING', '200 OK')
-        if headers in SIP_METHODS:
-            return headers.strip()
-        elif headers in SIP_RESPONSES:
+    def _normalize_headers(self, headers: str) -> str:
+        """
+        normalize the headers
+        :param headers:
+        :return:
+        """
+        SIP_METHODS = ("INVITE", "UPDATE", "ACK", "BYE", "OPTIONS")
+        SIP_RESPONSES = ("100 TRYING", "180 RINGING", "200 OK")
+        if headers in SIP_METHODS or headers in SIP_RESPONSES:
             return headers.strip()
         else:
             return "OTHER::" + headers
-        return headers.strip()
 
-    def _normalize_call_id(self, call_id):
+    def _normalize_call_id(self, call_id: str) -> str:
+        """
+        normalize the call id
+        :param call_id:
+        :return:
+        """
         if not call_id or call_id == "":
             return "UNKNOWN::" + call_id
         return call_id.strip()
 
-    def _normalize_content_len(self, content_len):
+    def _normalize_content_len(self, content_len: str) -> int:
+        """
+        normalize the content length
+        :param content_len:
+        :return:
+        """
         if not content_len or content_len == "":
             return "INVALID::" + content_len
         else:
--- ingestion/sessionizer.py
+++ ingestion/sessionizer.py
@@ -6,14 +6,15 @@ from collections import defaultdict
 
 
 class Sessionizer:
-	def __init__(self):
-		self.sessions = defaultdict(list)
 
-	def process(self, msg: dict):
-		call_id = msg.get("call_id")
-		if not call_id:
-			return
-		self.sessions[call_id].append(msg)
+    def __init__(self):
+        self.sessions = defaultdict(list)
 
-	def get_sessions(self):
-		return dict(self.sessions)
+    def process(self, msg: dict) -> defaultdict[str, dict]:
+        call_id = msg.get("call_id")
+        if not call_id or call_id.startswith("UNKNOWN"):
+            return
+        self.sessions[call_id].append(msg)
+
+    def get_sessions(self) -> dict[str, dict]:
+        return dict(self.sessions)
