08b37396a58a0c667d61299df09db3a2672ad343
n.maheshwar <nmnm.1.2007@gmail.com>
2026-04-11 18:07:11 +0530

    renamed auto gen files

--- ingestion/chunker_auto.py
+++ ingestion/chunker_auto.py
@@ -1,5 +1,5 @@
-from typing import List, Dict
 import uuid
+from typing import Dict, List
 
 
 class Chunker:
--- ingestion/normalizer_auto.py
+++ ingestion/normalizer_auto.py
@@ -1,5 +1,5 @@
-from typing import List, Dict
 from datetime import datetime
+from typing import Dict, List
 
 
 class Normalizer:
